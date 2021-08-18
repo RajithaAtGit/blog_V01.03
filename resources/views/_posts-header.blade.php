a33056bc51c4b93164f15733c3224cf423af8a7b
Goonawardana R N W <gtrxone@gmail.com>
2021-08-18 13:08:26 +0530

    Implement Search functionality
    - Create PostController
    - Add queryScope to Post Model
    
    Signed-off-by: Goonawardana R N W <gtrxone@gmail.com>

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -59,10 +59,15 @@
 
     <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-            <form method="GET" action="#">
+            <form method="GET" action="">
                 <label>
-                    <input type="text" name="search" placeholder="Find something"
-                           class="bg-transparent placeholder-black font-semibold text-sm">
+                    <input
+                        type="text"
+                        name="search"
+                        placeholder="Find something"
+                        class="bg-transparent placeholder-black font-semibold text-sm"
+                        value="{{ request('search') }}"
+                    >
                 </label>
             </form>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -16,27 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    /* debug Log sql and bindings --> logs save in Storage->logs->laravel.log*/
-    /*\Illuminate\Support\Facades\DB::listen(function ($query){
-        logger($query->sql,$query->bindings);
-    });*/
-
-    return view('posts',
-        [
-            'posts' => Post::query()->latest()->get(),
-            'categories' => Category::all()
-        ]);
-})->name('home');
-
-Route::get('/posts/{post:slug}', function (Post $post) {
-    return view('post',
-        [
-            'post' => $post
-
-        ]
-    );
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts',
